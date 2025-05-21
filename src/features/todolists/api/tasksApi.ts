53e87deedf36baf47ff9cff6c801de3422c2ce6b
Sergei-SLS <lyamin.sls@gmail.com>
2025-05-21 19:33:59 +0500

    style: set Prettier

--- src/features/todolists/api/tasksApi.ts
+++ src/features/todolists/api/tasksApi.ts
@@ -1,20 +1,19 @@
-import {instance} from "@/common/instance";
-import {DomainTask, GetTasksResponse, UpdateTaskModel} from "@/features/todolists/api/tasksApi.types.ts";
-import {BaseResponse} from "@/common/types";
-
+import { instance } from "@/common/instance"
+import { DomainTask, GetTasksResponse, UpdateTaskModel } from "@/features/todolists/api/tasksApi.types.ts"
+import { BaseResponse } from "@/common/types"
 
 export const tasksApi = {
-    getTasks(todolistId: string) {
-        return instance.get<GetTasksResponse>(`/todo-lists/${todolistId}/tasks`)
-    },
-    createTasks ({todolistId, title}: {todolistId: string; title: string}) {
-        return instance.post<BaseResponse<{item: DomainTask}>>(`/todo-lists/${todolistId}/tasks`, {title})
-    },
-    updateTask (payload: {todolistId: string; taskId: string; model: UpdateTaskModel}) {
-        const {todolistId, taskId, model} = payload
-        return instance.put<BaseResponse<{ item: DomainTask }>>(`/todo-lists/${todolistId}/tasks/${taskId}`, model)
-    },
-    deleteTask ({todolistId, taskId}: {todolistId: string; taskId: string}) {
-        return instance.delete<BaseResponse<{}>>(`/todo-lists/${todolistId}/tasks/${taskId}`)
-    },
+  getTasks(todolistId: string) {
+    return instance.get<GetTasksResponse>(`/todo-lists/${todolistId}/tasks`)
+  },
+  createTasks({ todolistId, title }: { todolistId: string; title: string }) {
+    return instance.post<BaseResponse<{ item: DomainTask }>>(`/todo-lists/${todolistId}/tasks`, { title })
+  },
+  updateTask(payload: { todolistId: string; taskId: string; model: UpdateTaskModel }) {
+    const { todolistId, taskId, model } = payload
+    return instance.put<BaseResponse<{ item: DomainTask }>>(`/todo-lists/${todolistId}/tasks/${taskId}`, model)
+  },
+  deleteTask({ todolistId, taskId }: { todolistId: string; taskId: string }) {
+    return instance.delete<BaseResponse<{}>>(`/todo-lists/${todolistId}/tasks/${taskId}`)
+  },
 }

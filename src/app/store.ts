58cf187032dcc006c678a45a06cc2904a26943ab
Sergei-SLS <lyamin.sls@gmail.com>
2025-05-23 22:18:57 +0500

    refactor: implemented use of createSlice for tasks and external actions processed

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,6 +1,6 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit"
 import { appReducer } from "./app-slice.ts"
-import { tasksReducer } from "@/features/todolists/model/tasks-reducer"
+import { tasksReducer } from "@/features/todolists/model/tasks-slice.ts"
 import { todolistsReducer } from "@/features/todolists/model/todolists-slice.ts"
 
 // объединение reducer'ов с помощью combineReducers

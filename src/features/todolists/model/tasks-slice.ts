37ceef29e4ee2e6ee5af68f4b45edba46c51a4e5
Sergei-SLS <lyamin.sls@gmail.com>
2025-05-29 21:14:03 +0500

    refactor: thunk for get tasks

--- src/features/todolists/model/tasks-slice.ts
+++ src/features/todolists/model/tasks-slice.ts
@@ -1,13 +1,32 @@
-import { createSlice, nanoid } from "@reduxjs/toolkit"
 import { createTodolistTC, deleteTodolistTC } from "./todolists-slice.ts"
+import { createAppSlice } from "@/common/utils"
+import { tasksApi } from "@/features/todolists/api/tasksApi.ts"
+import { TaskPriority, TaskStatus } from "@/common/enums/enums.ts"
+import { DomainTask } from "../api/tasksApi.types.ts"
+import { nanoid } from "@reduxjs/toolkit"
 
-export const tasksSlice = createSlice({
+export const tasksSlice = createAppSlice({
   name: "tasks",
   initialState: {} as TasksState,
   selectors: {
     selectTasks: (state) => state,
   },
   reducers: (create) => ({
+    fetchTasksTC: create.asyncThunk(
+      async (todolistId: string, thunkAPI) => {
+        try {
+          const res = await tasksApi.getTasks(todolistId)
+          return { todolistId, tasks: res.data.items }
+        } catch (error) {
+          return thunkAPI.rejectWithValue(null)
+        }
+      },
+      {
+        fulfilled: (state, action) => {
+          state[action.payload.todolistId] = action.payload.tasks
+        },
+      },
+    ),
     deleteTaskAC: create.reducer<{ todolistId: string; taskId: string }>((state, action) => {
       const tasks = state[action.payload.todolistId]
       const index = tasks.findIndex((task) => task.id === action.payload.taskId)
@@ -16,13 +35,24 @@ export const tasksSlice = createSlice({
       }
     }),
     createTaskAC: create.reducer<{ todolistId: string; title: string }>((state, action) => {
-      const newTask: Task = { title: action.payload.title, isDone: false, id: nanoid() }
+      const newTask: DomainTask = {
+        description: "",
+        title: action.payload.title,
+        status: TaskStatus.New,
+        priority: TaskPriority.Low,
+        startDate: "",
+        deadline: "",
+        id: nanoid(),
+        todoListId: action.payload.todolistId,
+        order: 0,
+        addedDate: "",
+      }
       state[action.payload.todolistId].unshift(newTask)
     }),
     changeTaskStatusAC: create.reducer<{ todolistId: string; taskId: string; isDone: boolean }>((state, action) => {
       const task = state[action.payload.todolistId].find((task) => task.id === action.payload.taskId)
       if (task) {
-        task.isDone = action.payload.isDone
+        task.status = action.payload.isDone ? TaskStatus.Completed : TaskStatus.New
       }
     }),
     changeTaskTitleAC: create.reducer<{ todolistId: string; taskId: string; title: string }>((state, action) => {
@@ -43,14 +73,8 @@ export const tasksSlice = createSlice({
   },
 })
 
-export const { deleteTaskAC, createTaskAC, changeTaskStatusAC, changeTaskTitleAC } = tasksSlice.actions
+export const { fetchTasksTC, deleteTaskAC, createTaskAC, changeTaskStatusAC, changeTaskTitleAC } = tasksSlice.actions
 export const tasksReducer = tasksSlice.reducer
 export const { selectTasks } = tasksSlice.selectors
 
-export type Task = {
-  id: string
-  title: string
-  isDone: boolean
-}
-
-export type TasksState = Record<string, Task[]>
+export type TasksState = Record<string, DomainTask[]>
--- src/features/todolists/ui/Todolists/TodolistItem/Tasks/Tasks.tsx
+++ src/features/todolists/ui/Todolists/TodolistItem/Tasks/Tasks.tsx
@@ -1,8 +1,10 @@
-import { useAppSelector } from "@/common/hooks"
+import { useAppDispatch, useAppSelector } from "@/common/hooks"
 import { TaskItem } from "./TaskItem/TaskItem"
 import List from "@mui/material/List"
 import { DomainTodolist } from "@/features/todolists/model/todolists-slice.ts"
-import { selectTasks } from "@/features/todolists/model/tasks-slice.ts"
+import { fetchTasksTC, selectTasks } from "@/features/todolists/model/tasks-slice.ts"
+import { useEffect } from "react"
+import { TaskStatus } from "@/common/enums/enums"
 
 type Props = {
   todolist: DomainTodolist
@@ -13,14 +15,20 @@ export const Tasks = ({ todolist }: Props) => {
 
   const tasks = useAppSelector(selectTasks)
 
+  const dispatch = useAppDispatch()
+
   const todolistTasks = tasks[id]
   let filteredTasks = todolistTasks
   if (filter === "active") {
-    filteredTasks = todolistTasks.filter((task) => !task.isDone)
+    filteredTasks = todolistTasks.filter((task) => task.status === TaskStatus.New)
   }
   if (filter === "completed") {
-    filteredTasks = todolistTasks.filter((task) => task.isDone)
+    filteredTasks = todolistTasks.filter((task) => task.status === TaskStatus.Completed)
   }
+
+  useEffect(() => {
+    dispatch(fetchTasksTC(id))
+  }, [])
 
   return (
     <>

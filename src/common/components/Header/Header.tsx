1a6c47bbe5c1694958de773a14d85f074044f539
Sergei-SLS <lyamin.sls@gmail.com>
2025-06-01 15:26:19 +0500

    style: add linearProgress

--- src/common/components/Header/Header.tsx
+++ src/common/components/Header/Header.tsx
@@ -10,6 +10,7 @@ import Container from "@mui/material/Container"
 import IconButton from "@mui/material/IconButton"
 import Switch from "@mui/material/Switch"
 import Toolbar from "@mui/material/Toolbar"
+import LinearProgress from "@mui/material/LinearProgress"
 
 export const Header = () => {
   const themeMode = useAppSelector(selectThemeMode)
@@ -37,6 +38,7 @@ export const Header = () => {
           </div>
         </Container>
       </Toolbar>
+      <LinearProgress />
     </AppBar>
   )
 }

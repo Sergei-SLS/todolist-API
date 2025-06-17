aa79c301bf78be9e9d42244097c06cf39f6ac32b
Sergei-SLS <lyamin.sls@gmail.com>
2025-06-17 21:12:53 +0500

    feat: add route PageNotFound

--- src/common/routing/Routing.tsx
+++ src/common/routing/Routing.tsx
@@ -1,15 +1,18 @@
 import { Main } from "@/app/Main"
 import { Route, Routes } from "react-router"
 import { Login } from "@/features/features/auth/ui/Login/Login.tsx"
+import { PageNotFound } from "@/common/components/PageNotFound"
 
 export const Path = {
   Main: "/",
   Login: "Login",
+  NotFound: "*",
 } as const
 
 export const Routing = () => (
   <Routes>
     <Route path={Path.Main} element={<Main />} />
     <Route path={Path.Login} element={<Login />} />
+    <Route path={Path.NotFound} element={<PageNotFound />} />
   </Routes>
 )

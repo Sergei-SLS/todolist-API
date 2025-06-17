fdbb5b2b1aac90cc824fc94783eaa33a434d4600
Sergei-SLS <lyamin.sls@gmail.com>
2025-06-17 21:01:14 +0500

    feat: add router login

--- src/common/routing/Routing.tsx
+++ src/common/routing/Routing.tsx
@@ -1,8 +1,15 @@
 import { Main } from "@/app/Main"
 import { Route, Routes } from "react-router"
+import { Login } from "@/features/features/auth/ui/Login/Login.tsx"
+
+export const Path = {
+  Main: "/",
+  Login: "Login",
+} as const
 
 export const Routing = () => (
   <Routes>
-    <Route path="/" element={<Main />} />
+    <Route path={Path.Main} element={<Main />} />
+    <Route path={Path.Login} element={<Login />} />
   </Routes>
 )

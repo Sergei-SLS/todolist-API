d9c67b66c1cc80d39cca78204b129b44f2a1e41d
Sergei-SLS <lyamin.sls@gmail.com>
2025-06-24 21:08:39 +0500

    feat: create button redirecting to the main page

--- src/common/components/PageNotFound/PageNotFound.tsx
+++ src/common/components/PageNotFound/PageNotFound.tsx
@@ -1,8 +1,21 @@
 import styles from "./PageNotFound.module.css"
+import { Link } from "react-router"
+import Button from "@mui/material/Button"
 
 export const PageNotFound = () => (
   <>
     <h1 className={styles.title}>404</h1>
     <h2 className={styles.subtitle}>page not found</h2>
+
+    <Button
+      variant="contained"
+      color="primary"
+      component={Link}
+      to={"/"}
+      sx={{ display: "flex", mx: "auto", mt: 3, width: 200 }}
+    >
+      {" "}
+      Go to Home{" "}
+    </Button>
   </>
 )

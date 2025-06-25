cb3320447f6100bd722afef37641bdeb33f31348
Sergei-SLS <lyamin.sls@gmail.com>
2025-06-25 19:19:11 +0500

    refactor: token set in localStorage

--- src/features/features/auth/model/auth-slice.ts
+++ src/features/features/auth/model/auth-slice.ts
@@ -1,6 +1,9 @@
 import { createAppSlice, handleServerNetworkError } from "@/common/utils"
 import { LoginInputs } from "@/features/features/auth/lib/schemas"
 import { authApi } from "@/features/features/auth/api/authApi.ts"
+import { AUTH_TOKEN } from "@/common/constants"
+import { changeStatusAC } from "@/app/app-slice"
+import { ResultCode } from "@/common/enums/enums.ts"
 
 export const authSlice = createAppSlice({
   name: "auth",
@@ -14,10 +17,12 @@ export const authSlice = createAppSlice({
     loginTC: create.asyncThunk(
       async (data: LoginInputs, { dispatch, rejectWithValue }) => {
         try {
-          await authApi.login(data)
-          // if (res.data.resultCode === ResultCode.Success) {
-          //   dispatch(setAppErrorAC({ status: "succeeded" }))
-          // }
+          dispatch(changeStatusAC({ status: "loading" }))
+          const res = await authApi.login(data)
+          if (res.data.resultCode === ResultCode.Success) {
+            dispatch(changeStatusAC({ status: "succeeded" }))
+          }
+          localStorage.setItem(AUTH_TOKEN, res.data.data.token)
           return { isLoggedIn: true }
         } catch (error) {
           handleServerNetworkError(dispatch, error)

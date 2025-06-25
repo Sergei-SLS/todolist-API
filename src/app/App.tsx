79228a37b2e0aa81b5b891e5228563152fcc15d0
Sergei-SLS <lyamin.sls@gmail.com>
2025-06-25 21:22:02 +0500

    feat: CircularProgress is visible when the application starts

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-import "./App.css"
+import styles from "./App.module.css"
 import { Header } from "@/common/components/Header/Header"
 import { useAppDispatch, useAppSelector } from "@/common/hooks"
 import { getTheme } from "@/common/theme"
@@ -8,22 +8,35 @@ import { selectThemeMode } from "@/app/app-slice.ts"
 import { ErrorSnackbar } from "@/common/components"
 import { Routing } from "@/common/routing"
 import { initializeAppTC } from "@/features/features/auth/model/auth-slice.ts"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
+import CircularProgress from "@mui/material/CircularProgress"
 
 export const App = () => {
-  const themeMode = useAppSelector(selectThemeMode)
+  const [isInitialized, setIsInitialized] = useState(false)
 
-  const dispatch = useAppDispatch()
+  const themeMode = useAppSelector(selectThemeMode)
 
   const theme = getTheme(themeMode)
 
+  const dispatch = useAppDispatch()
+
   useEffect(() => {
-    dispatch(initializeAppTC)
+    dispatch(initializeAppTC()).finally(() => {
+      setIsInitialized(true)
+    })
   }, [])
+
+  if (!isInitialized) {
+    return (
+      <div className={styles.circularProgressContainer}>
+        <CircularProgress size={150} thickness={3} />
+      </div>
+    )
+  }
 
   return (
     <ThemeProvider theme={theme}>
-      <div className={"app"}>
+      <div className={styles.app}>
         <CssBaseline />
         <Header />
         <Routing />

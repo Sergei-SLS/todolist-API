7d015550438176122f15f199483938b30d96ed6f
Sergei-SLS <lyamin.sls@gmail.com>
2025-06-27 22:18:29 +0500

    refactor: add universal protectedRoutes

--- src/common/components/ProtectedRoute/ProtectedRoute.tsx
+++ src/common/components/ProtectedRoute/ProtectedRoute.tsx
@@ -1,17 +1,14 @@
-import { useAppSelector } from "@/common/hooks"
-import { selectIsLoggedIn } from "@/features/features/auth/model/auth-slice.ts"
 import { Navigate } from "react-router"
 import { Path } from "@/common/routing/Routing.tsx"
 import { ReactNode } from "react"
 
 type Props = {
   children: ReactNode
+  isAllowed: boolean
 }
 
-export const ProtectedRoute = ({ children }: Props) => {
-  const isLoggedIn = useAppSelector(selectIsLoggedIn)
-
-  if (!isLoggedIn) {
+export const ProtectedRoute = ({ children, isAllowed }: Props) => {
+  if (!isAllowed) {
     return <Navigate to={Path.Login} />
   }
   return children
--- src/common/routing/Routing.tsx
+++ src/common/routing/Routing.tsx
@@ -3,6 +3,8 @@ import { Route, Routes } from "react-router"
 import { Login } from "@/features/features/auth/ui/Login/Login.tsx"
 import { PageNotFound } from "@/common/components/PageNotFound"
 import { ProtectedRoute } from "@/common/components/ProtectedRoute/ProtectedRoute.tsx"
+import { useAppSelector } from "@/common/hooks"
+import { selectIsLoggedIn } from "@/features/features/auth/model/auth-slice.ts"
 
 export const Path = {
   Main: "/",
@@ -10,17 +12,21 @@ export const Path = {
   NotFound: "*",
 } as const
 
-export const Routing = () => (
-  <Routes>
-    <Route
-      path={Path.Main}
-      element={
-        <ProtectedRoute>
-          <Main />
-        </ProtectedRoute>
-      }
-    />
-    <Route path={Path.Login} element={<Login />} />
-    <Route path={Path.NotFound} element={<PageNotFound />} />
-  </Routes>
-)
+export const Routing = () => {
+  const isLoggedIn = useAppSelector(selectIsLoggedIn)
+
+  return (
+    <Routes>
+      <Route
+        path={Path.Main}
+        element={
+          <ProtectedRoute isAllowed={isLoggedIn}>
+            <Main />
+          </ProtectedRoute>
+        }
+      />
+      <Route path={Path.Login} element={<Login />} />
+      <Route path={Path.NotFound} element={<PageNotFound />} />
+    </Routes>
+  )
+}

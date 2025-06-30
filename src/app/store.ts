0542fdcdaa441d46bccb5421c033d886497940e0
Sergei-SLS <lyamin.sls@gmail.com>
2025-06-30 22:08:02 +0500

    feat: add code splitting

--- src/app/store.ts
+++ src/app/store.ts
@@ -3,8 +3,8 @@ import { appReducer, appSlice } from "./app-slice.ts"
 import { tasksReducer, tasksSlice } from "@/features/todolists/model/tasks-slice.ts"
 import { todolistsReducer, todolistsSlice } from "@/features/todolists/model/todolists-slice.ts"
 import { authReducer, authSlice } from "@/features/features/auth/model/auth-slice.ts"
-import { todolistsApi } from "@/features/todolists/api/todolistsApi.ts"
 import { setupListeners } from "@reduxjs/toolkit/query"
+import { baseApi } from "@/features/todolists/api/baseApi.ts"
 
 // объединение reducer'ов с помощью combineReducers
 /*const rootReducer = combineReducers({
@@ -20,9 +20,9 @@ export const store = configureStore({
     [todolistsSlice.name]: todolistsReducer,
     [appSlice.name]: appReducer,
     [authSlice.name]: authReducer,
-    [todolistsApi.reducerPath]: todolistsApi.reducer,
+    [baseApi.reducerPath]: baseApi.reducer,
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(todolistsApi.middleware),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(baseApi.middleware),
 })
 
 setupListeners(store.dispatch)

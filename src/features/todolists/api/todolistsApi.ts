3f337c7f727a25d3f40ed1a3c642a55cbcd4c9c6
Sergei-SLS <lyamin.sls@gmail.com>
2025-06-30 21:03:02 +0500

    refactor: create request to addTodolist using RTK Query

--- src/features/todolists/api/todolistsApi.ts
+++ src/features/todolists/api/todolistsApi.ts
@@ -2,6 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 import { AUTH_TOKEN } from "@/common/constants"
 import { Todolist } from "@/features/todolists/api/todolistsApi.types.ts"
 import { DomainTodolist } from "@/features/todolists/model/todolists-slice.ts"
+import { BaseResponse } from "@/common/types"
 
 export const todolistsApi = createApi({
   reducerPath: "todolistsApi",
@@ -18,7 +19,14 @@ export const todolistsApi = createApi({
       transformResponse: (todolists: Todolist[]): DomainTodolist[] =>
         todolists.map((todolist) => ({ ...todolist, filter: "all", entityStatus: "idle" })),
     }),
+    addTodolist: build.mutation<BaseResponse<{ item: Todolist }>, string>({
+      query: (title) => ({
+        url: "todo-lists",
+        method: "POST",
+        body: { title },
+      }),
+    }),
   }),
 })
 
-export const { useGetTodolistsQuery } = todolistsApi
+export const { useGetTodolistsQuery, useAddTodolistMutation } = todolistsApi

d2564d4af9e8ce935ef9c1b5f78a6ca3d4ae0202
Sergei-SLS <lyamin.sls@gmail.com>
2025-07-03 21:23:29 +0500

    refactor: implemented createTask using RTK query

--- src/features/todolists/api/tasksApi.ts
+++ src/features/todolists/api/tasksApi.ts
@@ -1,19 +1,16 @@
-import { GetTasksResponse } from "@/features/todolists/api/tasksApi.types.ts"
+import { DomainTask, GetTasksResponse } from "@/features/todolists/api/tasksApi.types.ts"
 import { baseApi } from "@/features/todolists/api/baseApi.ts"
 import { BaseResponse } from "@/common/types"
-import { string } from "zod"
 
 export const tasksApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
     getTasks: builder.query<GetTasksResponse, string>({
       query: (todolistId) => `/todo-lists/${todolistId}/tasks`,
     }),
-    createTask: builder.mutation<BaseResponse<item: DomainTask>, string>({
-  query: (title) => ({method: 'post',
-    url: `/todo-lists/${todolistId}/tasks`,
-  body: {title}})
-})
+    createTask: builder.mutation<BaseResponse<{ item: DomainTask }>, { todolistId: string; title: string }>({
+      query: ({ todolistId, title }) => ({ method: "post", url: `/todo-lists/${todolistId}/tasks`, body: { title } }),
+    }),
   }),
 })
 
-export const { useGetTasksQuery } = tasksApi
+export const { useGetTasksQuery, useCreateTaskMutation } = tasksApi

07294a81aaca765b7d8857fa26dc3aa29bbd2e36
Sergei-SLS <lyamin.sls@gmail.com>
2025-07-08 22:00:08 +0500

    feat: skeletons are displayed during task loading

--- src/features/todolists/ui/Todolists/TodolistItem/Tasks/Tasks.tsx
+++ src/features/todolists/ui/Todolists/TodolistItem/Tasks/Tasks.tsx
@@ -3,6 +3,7 @@ import List from "@mui/material/List"
 import { DomainTodolist } from "@/features/todolists/model/todolists-slice.ts"
 import { TaskStatus } from "@/common/enums/enums"
 import { useGetTasksQuery } from "@/features/todolists/api/tasksApi.ts"
+import { TasksSkeleton } from "@/features/todolists/ui/Todolists/TodolistItem/Tasks/TasksSkeleton/TasksSkeleton.tsx"
 
 type Props = {
   todolist: DomainTodolist
@@ -11,7 +12,11 @@ type Props = {
 export const Tasks = ({ todolist }: Props) => {
   const { id, filter } = todolist
 
-  const { data } = useGetTasksQuery(id)
+  const { data, isLoading } = useGetTasksQuery(id)
+
+  if (isLoading) {
+    return <TasksSkeleton />
+  }
 
   const todolistTasks = data?.items ?? []
   let filteredTasks = todolistTasks

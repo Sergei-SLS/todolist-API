9c522c196e5001e7606bbcbb71c5c84faa701973
Sergei-SLS <lyamin.sls@gmail.com>
2025-07-08 22:07:05 +0500

    feat: add check for pending action using matchPending

--- src/app/appSlice.ts
+++ src/app/appSlice.ts
@@ -1,5 +1,7 @@
 import { createSlice, isFulfilled, isPending, isRejected } from "@reduxjs/toolkit"
 import { RequestStatus } from "@/common/types"
+import { todolistsApi } from "@/features/todolists/api/todolistsApi.ts"
+import { tasksApi } from "@/features/todolists/api/tasksApi"
 
 export const appSlice = createSlice({
   name: "app",
@@ -9,7 +11,13 @@ export const appSlice = createSlice({
   reducers: {},
   extraReducers: (builder) => {
     builder
-      .addMatcher(isPending, (state) => {
+      .addMatcher(isPending, (state, action) => {
+        if (
+          todolistsApi.endpoints.getTodolists.matchPending(action) ||
+          tasksApi.endpoints.getTasks.matchPending(action)
+        ) {
+          return
+        }
         state.status = "loading"
       })
       .addMatcher(isFulfilled, (state) => {

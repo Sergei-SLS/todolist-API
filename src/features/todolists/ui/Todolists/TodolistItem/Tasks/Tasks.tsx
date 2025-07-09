cd276c497b240cfa1cacb3fa34e24238df127d4c
Sergei-SLS <lyamin.sls@gmail.com>
2025-07-09 20:53:08 +0500

    feat: when loading tasks with the wrong todolistId an error is visible

--- src/features/todolists/ui/Todolists/TodolistItem/Tasks/Tasks.tsx
+++ src/features/todolists/ui/Todolists/TodolistItem/Tasks/Tasks.tsx
@@ -4,6 +4,9 @@ import { DomainTodolist } from "@/features/todolists/model/todolists-slice.ts"
 import { TaskStatus } from "@/common/enums/enums"
 import { useGetTasksQuery } from "@/features/todolists/api/tasksApi.ts"
 import { TasksSkeleton } from "@/features/todolists/ui/Todolists/TodolistItem/Tasks/TasksSkeleton/TasksSkeleton.tsx"
+import { setAppErrorAC } from "@/app/app-slice.ts"
+import { useDispatch } from "react-redux"
+import { useEffect } from "react"
 
 type Props = {
   todolist: DomainTodolist
@@ -12,7 +15,19 @@ type Props = {
 export const Tasks = ({ todolist }: Props) => {
   const { id, filter } = todolist
 
-  const { data, isLoading } = useGetTasksQuery(id)
+  const { data, isLoading, error } = useGetTasksQuery(id)
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    if (!error) return
+    if ("status" in error) {
+      const errMsg = "error" in error ? error.error : JSON.stringify(error.data)
+      dispatch(setAppErrorAC({ error: errMsg }))
+    } else {
+      dispatch(setAppErrorAC({ error: error.message || "Some error occurred" }))
+    }
+  }, [error])
 
   if (isLoading) {
     return <TasksSkeleton />

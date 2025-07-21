0033de5bc8c26294feb731e0aaec639fdd522ce3
Sergei-SLS <lyamin.sls@gmail.com>
2025-07-21 19:53:26 +0500

    refactor: delete taskReducer and todolistReducer in store.ts

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,22 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit"
 import { appReducer, appSlice } from "./app-slice.ts"
-import { tasksReducer, tasksSlice } from "@/features/todolists/model/tasks-slice.ts"
-import { todolistsReducer, todolistsSlice } from "@/features/todolists/model/todolists-slice.ts"
 import { setupListeners } from "@reduxjs/toolkit/query"
 import { baseApi } from "@/features/todolists/api/baseApi.ts"
 
-// объединение reducer'ов с помощью combineReducers
-/*const rootReducer = combineReducers({
-  tasks: tasksReducer,
-  todolists: todolistsReducer,
-  app: appReducer,
-})*/
-
-// создание store
 export const store = configureStore({
   reducer: {
-    [tasksSlice.name]: tasksReducer,
-    [todolistsSlice.name]: todolistsReducer,
     [appSlice.name]: appReducer,
     [baseApi.reducerPath]: baseApi.reducer,
   },

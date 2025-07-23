f6ffd7f3b7967d460a0ae171df044432c8fe3c0b
Sergei-SLS <lyamin.sls@gmail.com>
2025-07-23 21:33:02 +0500

    refactor: updateTask in Api optimistic update handler with query parameters

--- src/features/todolists/api/tasksApi.ts
+++ src/features/todolists/api/tasksApi.ts
@@ -25,19 +25,28 @@ export const tasksApi = baseApi.injectEndpoints({
         url: `/todo-lists/${todolistId}/tasks/${taskId}`,
         body: model,
       }),
-      async onQueryStarted({ todolistId, taskId, model }, { dispatch, queryFulfilled }) {
-        const patchResult = dispatch(
-          tasksApi.util.updateQueryData("getTasks", { todolistId, params: { page: 1 } }, (state) => {
-            const index = state.items.findIndex((task) => task.id === taskId)
-            if (index !== -1) {
-              state.items[index] = { ...state.items[index], ...model }
-            }
-          }),
-        )
+      async onQueryStarted({ todolistId, taskId, model }, { dispatch, queryFulfilled, getState }) {
+        const cachedArgsForQuery = tasksApi.util.selectCachedArgsForQuery(getState(), "getTasks")
+
+        let patchResult: any[] = []
+        cachedArgsForQuery.forEach(({ params }) => {
+          patchResult.push(
+            dispatch(
+              tasksApi.util.updateQueryData("getTasks", { todolistId, params: { page: params.page } }, (state) => {
+                const index = state.items.findIndex((task) => task.id === taskId)
+                if (index !== -1) {
+                  state.items[index] = { ...state.items[index], ...model }
+                }
+              }),
+            ),
+          )
+        })
         try {
           await queryFulfilled
         } catch {
-          patchResult.undo()
+          patchResult.forEach((patchResult) => {
+            patchResult.undo()
+          })
         }
       },
       invalidatesTags: (_res, _err, { todolistId }) => [{ type: "Task", id: todolistId }],

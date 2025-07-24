7ad1c369dd410241adaccf2deb58249ef4ecaf03
Sergei-SLS <lyamin.sls@gmail.com>
2025-07-24 20:44:42 +0500

    refactor: Unused modules removed

--- src/features/todolists/ui/Todolists/TodolistItem/TodolistTitle/TodolistTitle.tsx
+++ src/features/todolists/ui/Todolists/TodolistItem/TodolistTitle/TodolistTitle.tsx
@@ -7,7 +7,6 @@ import {
   useRemoveTodolistMutation,
   useUpdateTodolistTitleMutation,
 } from "@/features/todolists/api/todolistsApi.ts"
-// import { RequestStatus } from "@/common/types"
 import { useAppDispatch } from "@/common/hooks/useAppDispatch"
 import { DomainTodolist } from "@/features/todolists/lib/types"
 
@@ -22,26 +21,6 @@ export const TodolistTitle = ({ todolist }: Props) => {
   const [updateTodolistTitle] = useUpdateTodolistTitleMutation()
 
   const dispatch = useAppDispatch()
-
-  // const changeTodolistStatus = (entityStatus: RequestStatus) => {
-  //   dispatch(
-  //     todolistsApi.util.updateQueryData("getTodolists", undefined, (state) => {
-  //       const todolist = state.find((todolist) => todolist.id === id)
-  //       if (todolist) {
-  //         todolist.entityStatus = entityStatus
-  //       }
-  //     }),
-  //   )
-  // }
-
-  // const deleteTodolist = () => {
-  //   changeTodolistStatus("loading")
-  //   removeTodolist(id)
-  //     .unwrap()
-  //     .catch(() => {
-  //       changeTodolistStatus("idle")
-  //     })
-  // }
 
   const deleteTodolist = async () => {
     const patchResult = dispatch(
